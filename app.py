a10f2d626ae537d38d86f6ac4cb687e7e0870ba9
Nigel Martin <nigelmartinuk@outlook.com>
2022-05-19 13:37:55 +0100

    initial push

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, redirect
+from views import le_views # get views from view.py without circular referencing
+
+#create the app
+app = Flask( __name__ )
+# dynamically register the website views using externally created 
+# blueprint object
+app.register_blueprint( le_views)  
+
+#if app creation was successful, start the website
+if __name__ == "__main__" :
+    app.run(debug=True, port=8000)
